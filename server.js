ae3843673757a1495c49907121e9766d08ae68a7
Vadym Bobenko <bastiklive@gmail.com>
2019-10-21 15:19:05 +0300

    Finished routes

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 const express = require('express');
 const app = express();
+const connectBD = require('./config/db');
 
-app.get('/', (req, res) => res.json({ msg: 'Wellcome to Contact-Keeper\'s API' }));
+// Connect DateBase
+connectBD();
+
+// Init middleware
+app.use(express.json({ extended: false }));
+
+app.get('/', (req, res) =>
+  res.json({ msg: "Wellcome to Contact-Keeper's API" })
+);
 
 // Define routs
 app.use('/api/users', require('./routes/users'));
@@ -9,4 +18,4 @@ app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server startrd on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
